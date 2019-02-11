aabed8e8383b67243f265d4b4a2312beb97c9467
wingzerocat <mikechenlou@gmail.com>
2019-02-11 17:48:29 -0600

    Added stars instead of just text. completed basic css styling

--- client/components/form-top.jsx
+++ client/components/form-top.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Stars from './stars.jsx';
 
 const Header = (props) => (
   <div className='header'>
     <div><span className="price">${props.info.nightlyPrice}</span> per night</div>
     <div className='reviews'>
-      <span> Stars: {props.info.stars} Reviews: {props.info.reviews}</span>
+      <Stars stars={props.info.stars} /> {props.info.reviews}
     </div>
   </div>
 )
