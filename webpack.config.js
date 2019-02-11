9e1bd7b4dcde2f7f4ff0c5132a39c573919a769f
wingzerocat <mikechenlou@gmail.com>
2019-02-11 14:52:27 -0600

    added scss loader to the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,20 @@ module.exports = {
             loader: 'css-loader'
           }
         ]
+      },
+      {
+        test : /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
       }
     ]
   }
