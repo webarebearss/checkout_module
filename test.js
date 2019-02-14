6488ed8271f1b21aaa3464162848579982471e85
wingzerocat <mikechenlou@gmail.com>
2019-02-13 20:32:24 -0600

    got a test with component to pass

--- test.js
+++ test.js
@@ -1,52 +1,11 @@
-process.env.NODE_ENV = 'test';
-const chai = require('chai');
-const chaiHttp = require('chai-http')
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Stars from './client/components/stars.jsx';
+import Adapter from 'enzyme-adapter-react-16';
 
-chai.use(chaiHttp);
+Enzyme.configure({ adapter: new Adapter() });
 
-const server = require('./server');
-const knex = require('./server/db.js');
-
-describe('Routes: posts', () => {
-  beforeEach(() => {
-    return knex.migrate.rollback()
-      .then(() => knex.migrate.latest())
-      .then(() => knex.seed.run())
-  });
-
-  afterEach(() => {
-    return knex.migrate.rollback();
-  });
-
-  describe('Get rooms', () => {
-    test('should return 1 result for each room query', async() => {
-      var res = await chai.request(server).get('/rooms/1');
-      expect(res.body.length).toEqual(1);
-      res = await chai.request(server).get('/rooms/2');
-      expect(res.body).toHaveLength(1);
-    });
-
-    test('should return an obj with room details', async() => {
-      var res = await chai.request(server).get('/rooms/3');
-      expect(res.body[0]).toHaveProperty('price');
-      expect(res.body[0]).toHaveProperty('stars');
-      expect(res.body[0]).toHaveProperty('reviews');
-      expect(res.body[0]).toHaveProperty('guests');
-      expect(res.body[0]).toHaveProperty('cleaningFee');
-      expect(res.body[0]).toHaveProperty('serviceFee');
-    });
-  });
-
-  describe('Get reservations', () => {
-    test('should return an array', async() => {
-      var res = await chai.request(server).get('/rooms/bookings/1');
-      console.log(res.body);
-      expect(res.body).toHaveLength(1);
-    });
-
-    test('should return an empty array if room has no reservations', async() => {
-      var res = await chai.request(server).get('/rooms/bookings/4');
-      expect(res.body).toHaveLength(0);
-    });
-  });
+test('', () => {
+  const wrapper = shallow(<Stars/>);
+  expect(wrapper.exists()).toBe(true);
 });
