5299b08b6372f1d15187a5f1258a45b5256d67b6
Kelly Hudiono <kellyhudiono@gmail.com>
2019-02-27 15:38:31 -0800

    set up mariaDB

--- db/seeds/development/seedtest.js
+++ db/seeds/development/seedtest.js
@@ -1,9 +1,9 @@
 const faker = require('faker');
 
-const createFakeData = () => ({
+const createFakeListings = () => ({
   price: faker.random.number({
     'min': 70,
-    'max': 150
+    'max': 250
   }),
   // stars: faker.random.number({
   //   'min': 1,
@@ -31,7 +31,7 @@ const createFakeData = () => ({
     'max': 3
   }),
   title: faker.name.firstName() + `'s ` + faker.company.catchPhraseAdjective() + ' Home',
-  address: faker.address.streetAddress(),
+  address: faker.address.city(),
   highlights: faker.lorem.paragraph(nb_sentences=faker.random.number({'min': 1, 'max': 4})),
   introDesc: faker.lorem.paragraph(nb_sentences=5, variable_nb_sentences=true),
   spaceDesc: faker.lorem.paragraphs(nb=faker.random.number({'min': 1, 'max': 6})),
@@ -39,38 +39,93 @@ const createFakeData = () => ({
   otherDesc: faker.lorem.paragraphs(nb=faker.random.number({'min': 1, 'max': 3}))
 });
 
+const createFakeBookings = () => ([
+  {
+    checkin: '04-14-2019',
+    checkout: '04-18-2019',
+    numGuests: faker.random.number({'min': 1, 'max': 8}),
+    total: faker.random.number({'min': 100, 'max': 3000}),
+    listing_id: faker.random.number({'min': 1, 'max': 100})
+  },
+  {
+    checkin: '03-20-2019',
+    checkout: '03-27-2019',
+    numGuests: faker.random.number({'min': 1, 'max': 8}),
+    total: faker.random.number({'min': 100, 'max': 3000}),
+    listing_id: faker.random.number({'min': 1, 'max': 100})
+  },
+  {
+    checkin: '03-05-2019',
+    checkout: '03-10-2019',
+    numGuests: faker.random.number({'min': 1, 'max': 8}),
+    total: faker.random.number({'min': 100, 'max': 3000}),
+    listing_id: faker.random.number({'min': 1, 'max': 100})
+  },
+  {
+    checkin: '03-11-2019',
+    checkout: '03-15-2019',
+    numGuests: faker.random.number({'min': 1, 'max': 8}),
+    total: faker.random.number({'min': 100, 'max': 3000}),
+    listing_id: faker.random.number({'min': 1, 'max': 100})
+  },
+  {
+    checkin: '03-29-2019',
+    checkout: '04-05-2019',
+    numGuests: faker.random.number({'min': 1, 'max': 8}),
+    total: faker.random.number({'min': 100, 'max': 3000}),
+    listing_id: faker.random.number({'min': 1, 'max': 100})
+  },
+  {
+    checkin: '03-29-2019',
+    checkout: '04-05-2019',
+    numGuests: faker.random.number({'min': 1, 'max': 8}),
+    total: faker.random.number({'min': 100, 'max': 3000}),
+    listing_id: faker.random.number({'min': 1, 'max': 100})
+  },
+  {
+    checkin: '06-15-2019',
+    checkout: '06-18-2019',
+    numGuests: faker.random.number({'min': 1, 'max': 8}),
+    total: faker.random.number({'min': 100, 'max': 3000}),
+    listing_id: faker.random.number({'min': 1, 'max': 100})
+  },
+  {
+    checkin: '04-29-2019',
+    checkout: '05-05-2019',
+    numGuests: faker.random.number({'min': 1, 'max': 8}),
+    total: faker.random.number({'min': 100, 'max': 3000}),
+    listing_id: faker.random.number({'min': 1, 'max': 100})
+  },
+  {
+    checkin: '04-20-2019',
+    checkout: '04-25-2019',
+    numGuests: faker.random.number({'min': 1, 'max': 8}),
+    total: faker.random.number({'min': 100, 'max': 3000}),
+    listing_id: faker.random.number({'min': 1, 'max': 100})
+  },
+  {
+    checkin: '05-29-2019',
+    checkout: '06-05-2019',
+    numGuests: faker.random.number({'min': 1, 'max': 8}),
+    total: faker.random.number({'min': 100, 'max': 3000}),
+    listing_id: faker.random.number({'min': 1, 'max': 100})
+  }
+])
+
 exports.seed = async function(knex, Promise) {
-  const fakeBookings = [
-    {
-      checkin: '02-14-2019',
-      checkout: '02-18-2019',
-      numGuests: 2,
-      total: 500,
-      listing_id: 1
-    },
-    {
-      checkin: '02-20-2019',
-      checkout: '02-27-2019',
-      numGuests: 3,
-      total: 888,
-      listing_id: 1
-    },
-    {
-      checkin: '03-01-2019',
-      checkout: '03-05-2019',
-      numGuests: 2,
-      total: 358,
-      listing_id: 2
-    }
-  ];
-  const fakeData = [];
+  const fakeListings = [];
+  let fakeBookings = [];
   const desiredFakeData = 100;
   for (let i = 0; i < desiredFakeData; i++) {
-    fakeData.push(createFakeData());
+    fakeListings.push(createFakeListings());
   }
+  let desiredBookings = faker.random.number({'min': Math.floor(desiredFakeData*0.01), 'max': Math.floor(desiredFakeData*0.04)});
+  for (let i = 0; i < desiredBookings; i++) {
+    fakeBookings = fakeBookings.concat(createFakeBookings());
+  }
+  console.log(fakeBookings.length);
   await knex('listings')
-    .insert(fakeData);
+    .insert(fakeListings);
   await knex('bookings')
     .insert(fakeBookings);
 };
-
